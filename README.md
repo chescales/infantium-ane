d1c767774f5c3803c2b071d47a0680b017c77d65
Marc Pomar <marc@faable.com>
2014-09-22 19:15:12 +0200

    Extension cleanup

--- README.md
+++ README.md
@@ -3,8 +3,27 @@ Infantium-ane
 
 Adobe Native Extension to connect your apps to the Infantium Platform
 
-Version: v2.2.2 (24 march 2014)
+Version: v2.2.13 (22 September 2014)
 
 ## How to build it
-    ./build_ane.sh
+	1. Import infantium-lib-actionscript on FlexBuilder 4.6 and build swf file
+	2. Run bash script ./build_ane.sh
+
 Check build folder for built ane plugin. Ensure you have Adobe (R) AIR (R) Developer Tool (ADT) installed on $PATH
+
+## Explanation of directories:
+	- /infantium-ane: This folder is the Adobe Air way to pack .swf library and .jar files together. 
+	- /infantium-ane/extension.xml: Main Adobe Native Extension definition XML.
+	- /infantium-ane/platformoptions.xml: Here is where android .jar dependencies are defined
+	- /infantium-lib-actionscript: Here is the ActionScript code for the extension, this is a FlashBuilder 4.6 project. Import the project, and compile it to get the .swf file.Then dun build_ane.sh, it will place previously compiled swf file on infantium-ane directory and make the ANE extension
+	- /build where final ANE extension is placed
+
+
+## Help
+	
+	* Need help? Send a mail to support@infantium.com
+
+## Authors & Contributions
+	
+	* Chesco Igual
+	* Marc Pomar
--- build_ane.sh
+++ build_ane.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-echo "Building ANE..."
+# Get version number
+VERSION_STRING=$(cat VERSION.txt)
+
+echo "Building ANE $VERSION_STRING..."
 mkdir -p build
-cp InfantiumLib/bin/InfantiumLib.swc build
-pushd build && unzip -o InfantiumLib.swc && popd
-pushd InfantiumANE
+
+# Unzip .swc file to create library.swf file
+cp infantium-lib-actionscript/bin/infantium-lib-actionscript.swc build
+pushd build && unzip -o infantium-lib-actionscript.swc && popd
+
+# Get .swf files and place it on infantium-ane folder
+pushd infantium-ane
 cp -f ../build/library.swf android/library.swf
 cp -f ../build/library.swf default/library.swf
-cp -f ../build/InfantiumLib.swc InfantiumLib.swc
-adt -package -target ane InfantiumLib.ane extension.xml -swc InfantiumLib.swc -platform Android-ARM -C android . -platform default -C default .
+cp -f ../build/infantium-lib-actionscript.swc infantium-lib-actionscript.swc
+
+# Finally generate ANE extension. Note: Ensure adt is on path
+adt -package -target ane Infantium.ane extension.xml -swc infantium-lib-actionscript.swc -platform Android-ARM -C android . -platform default -C default .
 popd
+
+# Delete previous compiled versions and put version string on file
 rm -f build/*
-mv -f InfantiumANE/InfantiumLib.ane build/InfantiumLib_$(cat VERSION.txt).ane
+mv -f infantium-ane/Infantium.ane build/Infantium_$VERSION_STRING.ane
 echo "Done!"
